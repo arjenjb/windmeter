959f51f3c5876168d8957e718af75f18ea97fa94
Arjen Brouwer <arjenjb@gmail.com>
2018-09-06 21:49:48 +0200

    auto-update impl

---
+++ knipperlicht.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+led_pin = 12
+delay = 0.12
+
+GPIO.setup(led_pin, GPIO.OUT)
+
+i = 10
+
+try:
+  while i > 0:
+    i -= 1
+    GPIO.output(led_pin, 1)
+    sleep(delay)
+    GPIO.output(led_pin, 0)
+    sleep(delay)
+except KeyboardInterrupt:
+  GPIO.cleanup()
