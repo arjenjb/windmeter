25f0ade491e1a4639ee2c747c2c6451f913043f1
Arjen Brouwer <arjenjb@gmail.com>
2018-09-06 20:25:56 +0000

    improved auto-updater

--- auto-update.sh
+++ auto-update.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd $DIR
+
+sudo -u arjen git fetch
+
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
@@ -8,7 +13,11 @@ if [ $LOCAL = $REMOTE ]; then
     echo "Up-to-date"
 elif [ $LOCAL = $BASE ]; then
     echo "Need to pull"
+    service windmeter stop
     ./knipperlicht.py
+    sudo -u arjen git pull
+    service windmeter start
+
 elif [ $REMOTE = $BASE ]; then
     echo "Need to push"
 else
